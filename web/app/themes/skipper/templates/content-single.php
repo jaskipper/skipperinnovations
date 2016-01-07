d42e1d8ae5c17fb69dc87ed2a3b21b849afb4fb5
Jason Skipper <jasonskipper@gmail.com>
2016-01-07 10:06:32 -0500

    adding socialsharing

--- web/app/themes/skipper/templates/content-single.php
+++ web/app/themes/skipper/templates/content-single.php
@@ -12,4 +12,5 @@
     </footer>
     <?php comments_template('/templates/comments.php'); ?>
   </article>
+  <?php get_template_part('templates/socialshare'); ?>
 <?php endwhile; ?>
---
+++ web/app/themes/skipper/templates/socialshare.php
@@ -0,0 +1,12 @@
+<div id="skipper-socialsharing" class="clearfix">
+  <div class="ssshare"><i class="fa fa-share-square-o"></i><span>Share This!</span></div>
+  <a target="_blank" href="https://twitter.com/intent/tweet?text=<?php the_title() ?>&url=<?php echo get_page_link() ?>&via=@jasonaskipper" class="btn-tweet btn" href="#"><i class="fa fa-twitter"></i></a>
+  <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo get_page_link() ?>" class="btn-facebook btn" href="#"><i class="fa fa-facebook"></i></a>
+  <a target="_blank" href="https://plus.google.com/share?url=<?php echo get_page_link() ?>" class="btn-google btn" href="#"><i class="fa fa-google-plus"></i></a>
+  <a target="_blank" href="https://www.linkedin.com/shareArticle?mini=true&url=<?php echo get_page_link() ?>&title=<?php the_title() ?>&summary=&source=<?php echo get_page_link() ?>" class="btn-linkedin btn" href="#"><i class="fa fa-linkedin"></i></a>
+  <a target="_blank" href="https://pinterest.com/pin/create/button/?url=<?php echo get_page_link() ?>&description=&media=YOUR-IMAGE-SRC" class="btn-pinterest btn" href="#"><i class="fa fa-pinterest"></i></a>
+  <a target="_blank" href="https://vk.com/share.php?url=<?php echo get_page_link() ?>&title=<?php the_title() ?>&description=&image=<?php echo get_page_link() ?>&noparse=true" class="btn-vk btn" href="#"><i class="fa fa-vk"></i></a>
+  <a target="_blank" href="https://www.xing-share.com/app/user?op=share;sc_p=xing-share;url=<?php echo get_page_link() ?>" class="btn-xing btn" href="#"><i class="fa fa-xing"></i></a>
+  <a target="_blank" href="http://www.tumblr.com/share/link?url=<?php echo get_page_link() ?>&description=" class="btn-tumblr btn" href="#"><i class="fa fa-tumblr"></i></a>
+  <a target="_blank" href="http://www.reddit.com/submit?url=<?php echo get_page_link() ?>&title=<?php the_title() ?>" class="btn-reddit btn" href="#"><i class="fa fa-reddit"></i></a>
+</div>
