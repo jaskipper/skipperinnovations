6de31a3d3e79a1023fc682e3a078d5b45a383f13
Jason Skipper <jasonskipper@gmail.com>
2016-01-08 10:24:15 -0500

    working with dropdown menu's and added privacy policy to footer

--- web/app/themes/skipper/templates/footer.php
+++ web/app/themes/skipper/templates/footer.php
@@ -4,7 +4,7 @@
       <?php dynamic_sidebar('sidebar-footer'); ?>
     </div>
     <div id="copyright" class="row">
-      <p class="col-md-6 col-sm-6">Copyright &copy; <?php echo date("Y") ?> - <a href="https://skipperinnovations.com">Skipper Innovations</a></p>
+      <p class="col-md-6 col-sm-6">Copyright &copy; <?php echo date("Y") ?> - <a href="https://skipperinnovations.com">Skipper Innovations</a> <i><a href="/privacy-policy"> (Privacy Policy)</a></i></p>
       <p class="col-md-6 col-sm-6 copy-right"><span class="glyphicon glyphicon-leaf brand-primary" aria-hidden="true"></span> Skipper Theme - Designed by <a href="https://www.facebook.com/jasonaskipper" target="_blank">Jason Skipper</a></p>
     </div>
   </div>
