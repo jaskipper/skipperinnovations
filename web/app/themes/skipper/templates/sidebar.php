151a2e179245a45aaa6908f32f07b38bf8e19131
Jason Skipper <jasonskipper@gmail.com>
2016-01-11 14:46:07 -0500

    adsense

--- web/app/themes/skipper/templates/sidebar.php
+++ web/app/themes/skipper/templates/sidebar.php
@@ -1,3 +1,13 @@
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- SkipperInnovations1 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-3807336390042990"
+     data-ad-slot="5969966869"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
 <h2 class="socialh2">Social Media</h2>
 <div id="fp-social" class="social-feed-container"></div>
 <?php dynamic_sidebar('sidebar-primary'); ?>
