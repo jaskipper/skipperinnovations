4caf184372bc6f077dc17f6748db29419634f5e7
Jason Skipper <jasonskipper@gmail.com>
2016-01-14 21:48:10 -0500

    fbinit

--- web/app/themes/skipper/base.php
+++ web/app/themes/skipper/base.php
@@ -14,9 +14,25 @@ use Roots\Sage\Wrapper;
         <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'sage'); ?>
       </div>
     <![endif]-->
-    <?php
-      get_template_part('templates/fbinit');
-    ?>
+
+    <script>
+      window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '1561058730782643',
+          xfbml      : true,
+          version    : 'v2.5'
+        });
+      };
+
+      (function(d, s, id){
+         var js, fjs = d.getElementsByTagName(s)[0];
+         if (d.getElementById(id)) {return;}
+         js = d.createElement(s); js.id = id;
+         js.src = "//connect.facebook.net/en_US/sdk.js";
+         fjs.parentNode.insertBefore(js, fjs);
+       }(document, 'script', 'facebook-jssdk'));
+    </script>
+
     <?php
       do_action('get_header');
       get_template_part('templates/header');
--- web/app/themes/skipper/templates/fbinit.php
+++
@@ -1,6 +0,0 @@
-FB.init({
-  appId      : '1561058730782643',
-  status     : true,
-  xfbml      : true,
-  version    : 'v2.4' // or v2.0, v2.1, v2.2, v2.3
-});
