565bfbd57bb95a78484998ba0c1c031d15a1dd46
Jason Skipper <jason@skipperinnovations.com>
2017-01-07 06:13:09 -0500

    rm piwik change piwik code

--- web/app/themes/skipper/templates/head.php
+++ web/app/themes/skipper/templates/head.php
@@ -3,4 +3,20 @@
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php wp_head(); ?>
+  <!-- Piwik -->
+  <script type="text/javascript">
+    var _paq = _paq || [];
+    // tracker methods like "setCustomDimension" should be called before "trackPageView"
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function() {
+      var u="//piwik.skipperinnovations.com/";
+      _paq.push(['setTrackerUrl', u+'piwik.php']);
+      _paq.push(['setSiteId', '1']);
+      var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+      g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+    })();
+  </script>
+  <noscript><p><img src="//piwik.skipperinnovations.com/piwik.php?idsite=1&rec=1" style="border:0;" alt="" /></p></noscript>
+  <!-- End Piwik Code -->
 </head>
